20e8158a9edf8a82188ff060e5d241f0ede204bb
Max de Vocht <maxdevocht@hotmail.com>
2024-02-29 15:40:37 +0100

    Update js
    Added tab btn function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const tabBtn = document.getElementById("tab-btn")
 const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
@@ -35,6 +36,14 @@ inputBtn.addEventListener("click", function(){
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     render(myLeads)
     console.log( localStorage.getItem("myLeads") )
+})
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
 })
 
 deleteBtn.addEventListener("click", function(){
