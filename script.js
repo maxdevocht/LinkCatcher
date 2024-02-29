3ef5e3570182c241649a2dfca87ab44316790f7c
Max de Vocht <maxdevocht@hotmail.com>
2024-02-29 15:48:59 +0100

    Update js
    finishing

--- script.js
+++ script.js
@@ -39,15 +39,12 @@ inputBtn.addEventListener("click", function(){
 })
 
 tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        if (tabs && tabs.length > 0) {
-            myLeads.push(tabs[0].url);
-            // ... rest of your code
-        } else {
-            // Handle the case where there are no active tabs
-            console.error("No active tab found");
-        }
-    });
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
 
 deleteBtn.addEventListener("click", function(){
     localStorage.clear()
