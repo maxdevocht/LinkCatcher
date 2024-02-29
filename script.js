c10f13e77b5bf5cba0875676b9ff5bf3f68b131b
Max de Vocht <maxdevocht@hotmail.com>
2024-02-29 14:43:53 +0100

    Update js
    Added delete btn in variable

--- script.js
+++ script.js
@@ -2,9 +2,10 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
 
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
